5a98d747531d2135c4ce4be637cffa8783661578
namilan <nichole.milan@gmail.com>
2019-06-25 16:26:07 -0400

    Final

--- src/main/resources/templates/js/app.js
+++ src/main/resources/templates/js/app.js
@@ -1,3 +1,31 @@
 function myFunction(){
     alert(document.getElementById("myP").style.cursor);
 }
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 9000);    
+}
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
